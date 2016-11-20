4f69349a4b72311d3b04d275226cd620b1242e0a
pavel <pavel>
2016-11-20 22:35:50 +0200

    1.1

--- gmapsN1/LakeMapVC.m
+++ gmapsN1/LakeMapVC.m
@@ -24,9 +24,20 @@
     [super viewDidLoad];
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:28.5382 longitude:-81.3687 zoom:14 bearing:0 viewingAngle:0];
     self.mapView = [GMSMapView mapWithFrame: self.view.bounds camera:camera];
+    self.mapView.mapType = kGMSTypeNormal;
+    self.mapView.myLocationEnabled = true;
+    self.mapView.settings.compassButton = true;
+    self.mapView.settings.myLocationButton = true;
+    [self.mapView setMinZoom:10 maxZoom:18];
     [self.view addSubview:self.mapView];
+    
+}
+- (BOOL)prefersStatusBarHidden {
+    return YES;
 }
 
-
-
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    self.mapView.padding = UIEdgeInsetsMake(self.topLayoutGuide.length + 5, 0, self.bottomLayoutGuide.length + 5, 0);
+}
 @end
